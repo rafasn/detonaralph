4f65b6e28804036ecb13469d2da2d10a05097890
rafasn <neiva.rafs@gmail.com>
2025-06-15 15:09:59 -0300

    final

--- SFC/js/engine.js
+++ SFC/js/engine.js
@@ -10,9 +10,27 @@ const state= {
         gameVelocity:1000,
         hitPosition:0,
         result:0,
+        currentTime: 60,
+    },
+    actions:{
+        countDownTimerId: setInterval(countDown,1000),
     },
 };
 
+function playSound(){
+    let audio = new Audio("../SFC/audios/hit.m4a");
+    audio.volume=0.2;
+    audio.play();
+}
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent= state.values.currentTime;
+
+    if(state.values.currentTime <=0) {
+        clearInterval(state.actions.countDownTimerId);
+        alert("Game Over! O seu resultado foi:" + state.values.result);
+    }
+}
 function randomSquare(){
     state.view.squares.forEach((square)=>{
         square.classList.remove("enemy");
@@ -36,6 +54,7 @@ function addListenerHitBox(){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound();
             }
         });
     });
